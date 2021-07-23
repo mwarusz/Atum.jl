73669a5fd9fc5176daaab3e841ae2d03a8436064
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2021-07-23 14:27:53 -0700

    Use SafeTestsets

--- test/advection/runtests.jl
+++ test/advection/runtests.jl
@@ -1,7 +1,8 @@
 using Test
+using SafeTestsets
 
 @testset "advection" begin
-  include("wave_1d.jl")
-  include("wave_2d.jl")
-  include("wave_3d.jl")
+  @safetestset "wave_1d" begin include("wave_1d.jl") end
+  @safetestset "wave_2d" begin include("wave_2d.jl") end
+  @safetestset "wave_3d" begin include("wave_3d.jl") end
 end
