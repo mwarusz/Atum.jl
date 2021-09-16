ae7c9d4a7a7ea9d43bc0e7a35bb6638f3454c155
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2021-09-16 11:46:27 -0700

    wip

--- paper_experiments/gravitywave/plot_gravitywave.jl
+++ paper_experiments/gravitywave/plot_gravitywave.jl
@@ -4,6 +4,7 @@ include("../helpers.jl")
 using FileIO
 using JLD2: @load
 using PyPlot
+using PGFPlotsX
 using LaTeXStrings
 
 rcParams!(PyPlot.PyDict(PyPlot.matplotlib."rcParams"))
@@ -13,12 +14,77 @@ function compute_errors(dg, diag, diag_exact)
   w_exact, δT_exact = components(diag_exact)
 
   err_w = weightednorm(dg, w .- w_exact) / sqrt(sum(dg.MJ))
-  err_δT = weightednorm(dg, δT .- δT_exact) / sqrt(sum(dg.MJ))
+  err_T = weightednorm(dg, δT .- δT_exact) / sqrt(sum(dg.MJ))
 
-  err_w, err_δT
+  err_w, err_T
+end
+function convergence_plot(outputdir, convergence_data)
+  dxs = convergence_data[3].dxs
+  @pgf begin
+    plotsetup = {
+              xlabel = "Δx [km]",
+              grid = "major",
+              xmode = "log",
+              ymode = "log",
+              xticklabel="{\\pgfmathparse{exp(\\tick)/1000}\\pgfmathprintnumber[fixed,precision=3]{\\pgfmathresult}}",
+              #xmax = 1,
+              xtick = dxs,
+              #ymin = 10. ^ -10 / 5,
+              #ymax = 5,
+              #ytick = 10. .^ -(0:2:10),
+              legend_pos="south east",
+              group_style= {group_size="2 by 2",
+                            vertical_sep="2cm",
+                            horizontal_sep="2cm"},
+            }
+
+    Ns = sort(collect(keys(convergence_data)))
+    ylabel = L"L_{2}" * " error"
+    fig = GroupPlot(plotsetup)
+    for s in ('T', 'w')
+        labels = []
+        plots = []
+        title = "$s convergence"
+        for N in Ns
+          @show N
+          dxs = convergence_data[N].dxs
+          if N == 2
+            Tcoeff = 2e-4
+            wcoeff = 3e-4
+          elseif N == 3
+            Tcoeff = 2e-5
+            wcoeff = 3e-5
+          elseif N == 4
+            Tcoeff = 5e-6
+            wcoeff = 8e-6
+          end
+          ordl = (dxs ./ dxs[end]) .^ (N + 1)
+          if s === 'T'
+            errs = convergence_data[N].T_errors
+            ordl *= Tcoeff
+          else
+            errs = convergence_data[N].w_errors
+            ordl *= wcoeff
+          end
+          coords = Coordinates(dxs, errs)
+          ordc = Coordinates(dxs, ordl)
+          plot = PlotInc({}, coords)
+          plotc = Plot({dashed}, ordc)
+          push!(plots, plot, plotc)
+          #push!(labels, "N$N " * @sprintf("(%.2f)", rates[end]))
+          push!(labels, "N$N")
+          push!(labels, "order $(N+1)")
+        end
+        legend = Legend(labels)
+        push!(fig, {title=title, ylabel=ylabel}, plots..., legend)
+      end
+      savepath = joinpath(outputdir,
+                          "gw_convergence.pdf")
+      pgfsave(savepath, fig)
+  end
 end
 
-function plot(root, diag_points, diag, diag_exact)
+function contour_plot(root, diag_points, diag, diag_exact)
   x, z = components(diag_points)
   FT = eltype(x)
   w, δT = components(diag)
@@ -60,6 +126,40 @@ function plot(root, diag_points, diag, diag_exact)
   tight_layout()
   savefig(joinpath(root, "gw_T_perturbation.pdf"))
   close(fig)
+
+  k = findfirst(z[1, :] .>= 5)
+  @show z[1, k]
+  x_k = x[:, k]
+  w_k = w[:, k]
+  w_exact_k = w_exact[:, k]
+  #linedata[(N, K...)] = (x_k, w_k, w_exact_k)
+  @pgf begin
+    fig = @pgf GroupPlot({group_style= {group_size="1 by 1", vertical_sep="1.5cm"},
+                         xmin=0,
+                         xmax= 300})
+    #x, w, w_exact = linedata[(3, 40, 10)]
+
+    ytick = [scaling * (-3 + i) * 1e-3 for i in 0:7]
+    xtick = [50 * i for i in 0:6]
+    p1 = Plot({color="blue"}, Coordinates(x_k, w_k))
+    p2 = Plot({}, Coordinates(x_k, w_exact_k))
+    push!(fig, {xlabel="x [km]",
+                ylabel="w [m/s]",
+                ytick = ytick,
+                xtick = xtick,
+                width="10cm",
+                height="5cm"},
+               p1, p2)
+    #x, w, w_exact = linedata[(3, 40, 10)]
+    #p1 = Plot({color="blue"}, Coordinates(x, w))
+    #p2 = Plot({}, Coordinates(x, w_exact))
+    #push!(fig, {xlabel="x [km]",
+    #            ylabel="w [m/s]",
+    #            width="10cm",
+    #            height="5cm"},
+    #           p1, p2)
+    pgfsave(joinpath(root, "gw_line.pdf"), fig)
+  end
 end
 
 function calculate_diagnostics(law, q, x⃗)
@@ -85,8 +185,7 @@ end
 let
   outputdir = joinpath("paper_output", "gravitywave")
 
-  errors_w = Dict()
-  errors_T = Dict()
+  convergence_data = Dict()
   for (root, dir, files) in walkdir(outputdir)
     jldfiles = filter(s->endswith(s, "jld2"), files)
     length(jldfiles) == 0 && continue
@@ -96,20 +195,44 @@ let
     law = data["law"]
     dg = data["dg"]
     grid = dg.grid
+    cell = referencecell(grid)
     q = data["q"]
     qexact = data["qexact"]
+    N = size(cell)[1] - 1
+    KX = size(grid)[1]
 
     diag = calculate_diagnostics.(Ref(law), q, points(grid))
     diag_exact = calculate_diagnostics.(Ref(law), qexact, points(grid))
 
-    dx = _L / size(grid)[1]
-    errors_w[dx], errors_T[dx] = compute_errors(dg, diag, diag_exact)
+    dx = _L / size(grid)[1] / 1e3
 
-    @show dx, errors_w[dx], errors_T[dx]
+    err_w, err_T = compute_errors(dg, diag, diag_exact)
+    @show dx, err_w, err_T
+
+    if N in keys(convergence_data)
+      push!(convergence_data[N].w_errors, err_w)
+      push!(convergence_data[N].T_errors, err_T)
+      push!(convergence_data[N].dxs, dx)
+    else
+      convergence_data[N] = (dxs = Float64[], w_errors=Float64[], T_errors=Float64[])
+      push!(convergence_data[N].w_errors, err_w)
+      push!(convergence_data[N].T_errors, err_T)
+      push!(convergence_data[N].dxs, dx)
+    end
 
     diag_points, diag = interpolate_equidistant(diag, grid)
     _, diag_exact = interpolate_equidistant(diag_exact, grid)
 
-    plot(root, diag_points, diag, diag_exact)
+    contour_plot(root, diag_points, diag, diag_exact)
   end
+
+  # sort convergence_data
+  for N in keys(convergence_data)
+    tpl = convergence_data[N]
+    tpl = sort(collect(zip(tpl...)))
+    convergence_data[N] = (dxs=[t[1] for t in tpl],
+                           w_errors = [t[2] for t in tpl],
+                           T_errors = [t[3] for t in tpl])
+  end
+  convergence_plot(outputdir, convergence_data)
 end
