d2288001c1891f9326b08816fb88cc1aa7acb456
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2021-09-20 17:47:27 -0700

    Wip on gw

--- paper_experiments/gravitywave/plot_gravitywave.jl
+++ paper_experiments/gravitywave/plot_gravitywave.jl
@@ -7,7 +7,29 @@ using PyPlot
 using PGFPlotsX
 using LaTeXStrings
 
+const _scaling = 1e2 * _ΔT
+
 rcParams!(PyPlot.PyDict(PyPlot.matplotlib."rcParams"))
+
+function diagnostics(law, q, x⃗)
+  ρ, ρu, ρw, ρe = q
+  ρu⃗ = SVector(ρu, ρw)
+
+  x, z = x⃗
+  Φ = grav(law) * z
+
+  R_d = gas_constant(law)
+  p = EulerGravity.pressure(law, ρ, ρu⃗, ρe, Φ)
+  T = p / (R_d * ρ)
+
+  ρ_ref, p_ref = referencestate(law, x⃗)
+  T_ref = p_ref / (R_d * ρ_ref)
+
+  w = ρw / ρ
+  δT = T - T_ref
+
+  SVector(w, δT)
+end
 
 function compute_errors(dg, diag, diag_exact)
   w, δT = components(diag)
@@ -18,6 +40,143 @@ function compute_errors(dg, diag, diag_exact)
 
   err_w, err_T
 end
+
+function calculate_diagnostics(outputdir)
+  convergence_data = Dict()
+  diagnostic_data = Dict()
+
+  for (root, dir, files) in walkdir(outputdir)
+    jldfiles = filter(s->endswith(s, "jld2"), files)
+    length(jldfiles) == 0 && continue
+    @assert length(jldfiles) == 1
+    data = load(joinpath(root, jldfiles[1]))
+
+    law = data["law"]
+    dg = data["dg"]
+    grid = dg.grid
+    cell = referencecell(grid)
+    q = data["q"]
+    qexact = data["qexact"]
+    N = size(cell)[1] - 1
+    KX = size(grid)[1]
+
+    diag = diagnostics.(Ref(law), q, points(grid))
+    diag_exact = diagnostics.(Ref(law), qexact, points(grid))
+
+    dx = _L / KX
+    err_w, err_T = compute_errors(dg, diag, diag_exact)
+    @show dx, err_w, err_T
+
+    if N in keys(convergence_data)
+      push!(convergence_data[N].w_errors, err_w)
+      push!(convergence_data[N].T_errors, err_T)
+      push!(convergence_data[N].dxs, dx)
+    else
+      convergence_data[N] = (dxs = Float64[], w_errors=Float64[], T_errors=Float64[])
+      push!(convergence_data[N].w_errors, err_w)
+      push!(convergence_data[N].T_errors, err_T)
+      push!(convergence_data[N].dxs, dx)
+    end
+
+    diag_points, diag = interpolate_equidistant(diag, grid)
+    _, diag_exact = interpolate_equidistant(diag_exact, grid)
+
+    x, z = components(diag_points)
+    # convert coordiantes to km
+    x ./= 1e3
+    z ./= 1e3
+
+    diagnostic_data[(N, KX)] = (diag_points, diag, diag_exact)
+  end
+
+  # sort convergence_data
+  for N in keys(convergence_data)
+    tpl = convergence_data[N]
+    tpl = sort(collect(zip(tpl...)))
+    convergence_data[N] = (dxs=[t[1] for t in tpl],
+                           w_errors = [t[2] for t in tpl],
+                           T_errors = [t[3] for t in tpl])
+  end
+
+  diagnostic_data, convergence_data
+end
+
+function contour_plot(root, diagnostic_data, N, KX)
+  diag_points, diag, diag_exact = diagnostic_data[(N, KX)]
+
+  x, z = components(diag_points)
+  FT = eltype(x)
+  w, δT = components(diag)
+  w_exact, δT_exact = components(diag_exact)
+
+  ioff()
+  xticks = range(0, 300, length = 7)
+  yticks = range(0, 10, length = 5)
+  fig, ax = subplots(2, 1, figsize=(14, 14))
+
+  for a in ax
+    a.set_xlim([0, 300])
+    a.set_xticks(xticks)
+    a.set_yticks(yticks)
+    a.set_ylim([0, 10])
+    a.set_xlabel(L"x" * " [km]")
+    a.set_ylabel(L"z" * " [km]")
+    a.set_aspect(15)
+  end
+
+  ll = 0.0036 * _scaling
+  sl = 0.0006 * _scaling
+  levels = vcat(-ll:sl:-sl, sl:sl:ll)
+  ax[1].set_title("T perturbation [K]")
+  norm = matplotlib.colors.TwoSlopeNorm(vmin=levels[1], vcenter=0, vmax=levels[end])
+  cset = ax[1].contourf(x', z', δT', cmap=ColorMap("PuOr"), levels=levels, norm=norm)
+  ax[1].contour(x', z', δT_exact', levels=levels, colors=("k",))
+  cbar = colorbar(cset, ax = ax[1], ticks=levels)
+
+  ax[2].set_title("w [m/s]")
+  #norm = matplotlib.colors.TwoSlopeNorm(vmin=levels[1], vcenter=0, vmax=levels[end])
+  cset = ax[2].contourf(x', z', w', cmap=ColorMap("PuOr"), levels=levels)
+  ax[2].contour(x', z', w_exact', levels=levels, colors=("k",))
+  cbar = colorbar(cset, ax = ax[2], ticks=levels)
+
+  tight_layout()
+  savefig(joinpath(root, "gw_contour_$(N)_$(KX).pdf"))
+  close(fig)
+end
+
+function line_plot(root, diagnostic_data, N, KX)
+  diag_points, diag, diag_exact = diagnostic_data[(N, KX)]
+
+  x, z = components(diag_points)
+  FT = eltype(x)
+  w, δT = components(diag)
+  w_exact, δT_exact = components(diag_exact)
+
+  k = findfirst(z[1, :] .>= 5)
+
+  @show z[1, k]
+  x_k = x[:, k]
+  w_k = w[:, k]
+  w_exact_k = w_exact[:, k]
+  @pgf begin
+    fig = @pgf GroupPlot({group_style= {group_size="1 by 1", vertical_sep="1.5cm"},
+                         xmin=0,
+                         xmax= 300})
+    ytick = [_scaling * (-3 + i) * 1e-3 for i in 0:7]
+    xtick = [50 * i for i in 0:6]
+    p1 = Plot({dashed}, Coordinates(x_k, w_k))
+    p2 = Plot({}, Coordinates(x_k, w_exact_k))
+    push!(fig, {xlabel="x [km]",
+                ylabel="w [m/s]",
+                ytick = ytick,
+                xtick = xtick,
+                width="10cm",
+                height="5cm"},
+               p1, p2)
+    pgfsave(joinpath(root, "gw_line_$(N)_$(KX).pdf"), fig)
+  end
+end
+
 function convergence_plot(outputdir, convergence_data)
   dxs = convergence_data[3].dxs
   @pgf begin
@@ -39,12 +198,13 @@ function convergence_plot(outputdir, convergence_data)
             }
 
     Ns = sort(collect(keys(convergence_data)))
-    ylabel = L"L_{2}" * " error"
     fig = GroupPlot(plotsetup)
     for s in ('T', 'w')
+        unit = s == 'T' ? "K" : "m/s"
+        ylabel = L"L_{2}" * " error of $s [$unit]"
         labels = []
         plots = []
-        title = "$s convergence"
+        title = "Convergence of $s"
         for N in Ns
           @show N
           dxs = convergence_data[N].dxs
@@ -84,155 +244,117 @@ function convergence_plot(outputdir, convergence_data)
   end
 end
 
-function contour_plot(root, diag_points, diag, diag_exact)
-  x, z = components(diag_points)
-  FT = eltype(x)
-  w, δT = components(diag)
-  w_exact, δT_exact = components(diag_exact)
 
-  # convert coordiantes to km
-  x ./= 1e3
-  z ./= 1e3
-
+function compare_contour_plot(root, diagnostics, N, (KX1, KX2))
   ioff()
-  xticks = range(0, 300, length = 7)
-  fig, ax = subplots(2, 1, figsize=(14, 14))
 
-  for a in ax
-    a.set_xlim([xticks[1], xticks[end]])
-    a.set_xticks(xticks)
-    a.set_xlabel(L"x" * " [km]")
-    a.set_ylabel(L"z" * " [km]")
-    a.set_aspect(15)
+  fig, ax = subplots(2, 2, figsize=(28, 10), sharex="col", sharey="row")
+
+  ll = 0.0036 * _scaling
+  sl = 0.0006 * _scaling
+  levels = vcat(-ll:sl:-sl, sl:sl:ll)
+  xticks = range(0, 300, length = 7)
+  yticks = range(0, 10, length = 5)
+
+  m = 1
+  cset = nothing
+  for KX in (KX1, KX2)
+    diag_points, diag, diag_exact = diagnostics[(N, KX)]
+    x, z = components(diag_points)
+    w, δT = components(diag)
+    w_exact, δT_exact = components(diag_exact)
+
+
+    dx = _L / KX
+
+    ax[1, m].set_title(L"\Delta x" * " = $(dx) m\n\n w [m/s]")
+    #norm = matplotlib.colors.TwoSlopeNorm(vmin=levels[1], vcenter=0, vmax=levels[end])
+    cset = ax[1, m].contourf(x', z', w', cmap=ColorMap("PuOr"), levels=levels)
+    ax[1, m].contour(x', z', w_exact', levels=levels, colors=("k",))
+
+    ax[2, m].set_title("T perturbation [K]")
+    norm = matplotlib.colors.TwoSlopeNorm(vmin=levels[1], vcenter=0, vmax=levels[end])
+    cset = ax[2, m].contourf(x', z', δT', cmap=ColorMap("PuOr"), levels=levels, norm=norm)
+    ax[2, m].contour(x', z', δT_exact', levels=levels, colors=("k",))
+
+
+    m += 1
   end
 
-  ΔT = FT(0.0001)
-  scaling = 1e2 * _ΔT
-  ll = 0.0036 * scaling
-  sl = 0.0006 * scaling
-  levels = vcat(-ll:sl:-sl, sl:sl:ll)
-  ax[1].set_title("T perturbation [K]")
-  norm = matplotlib.colors.TwoSlopeNorm(vmin=levels[1], vcenter=0, vmax=levels[end])
-  cset = ax[1].contourf(x', z', δT', cmap=ColorMap("PuOr"), levels=levels, norm=norm)
-  ax[1].contour(x', z', δT_exact', levels=levels, colors=("k",))
-  cbar = colorbar(cset, ax = ax[1])
+  for a in vec(ax)
+      a.set_xlim([0, 300])
+      a.set_xticks(xticks)
+      a.set_yticks(yticks)
+      a.set_ylim([0, 10])
+      a.set_aspect(10)
+  end
 
-  ax[2].set_title("w [m/s]")
-  #norm = matplotlib.colors.TwoSlopeNorm(vmin=levels[1], vcenter=0, vmax=levels[end])
-  cset = ax[2].contourf(x', z', w', cmap=ColorMap("PuOr"), levels=levels)
-  ax[2].contour(x', z', w_exact', levels=levels, colors=("k",))
-  cbar = colorbar(cset, ax = ax[2])
+  ax[2, 1].set_xlabel(L"x" * " [km]")
+  ax[2, 2].set_xlabel(L"x" * " [km]")
+  ax[1, 1].set_ylabel(L"z" * " [km]")
+  ax[2, 1].set_ylabel(L"z" * " [km]")
+
 
   tight_layout()
-  savefig(joinpath(root, "gw_T_perturbation.pdf"))
+  cbar = colorbar(cset, ax = vec(ax), shrink=1.0,
+                  ticks=levels)
+  savefig(joinpath(root, "gw_compare_contour_$(N)_$(KX1)_vs_$(KX2).pdf"))
   close(fig)
+end
 
-  k = findfirst(z[1, :] .>= 5)
-  @show z[1, k]
-  x_k = x[:, k]
-  w_k = w[:, k]
-  w_exact_k = w_exact[:, k]
-  #linedata[(N, K...)] = (x_k, w_k, w_exact_k)
-  @pgf begin
-    fig = @pgf GroupPlot({group_style= {group_size="1 by 1", vertical_sep="1.5cm"},
-                         xmin=0,
-                         xmax= 300})
-    #x, w, w_exact = linedata[(3, 40, 10)]
+function compare_line_plot(root, diagnostics, N, (KX1, KX2))
+  fig = @pgf GroupPlot({group_style= {group_size="2 by 1", horizontal_sep="1.5cm"},
+                        xmin=0,
+                        xmax= 300})
+  for KX in (KX1, KX2)
+    diag_points, diag, diag_exact = diagnostics[(N, KX)]
+    x, z = components(diag_points)
+    w, δT = components(diag)
+    w_exact, δT_exact = components(diag_exact)
 
-    ytick = [scaling * (-3 + i) * 1e-3 for i in 0:7]
+    k = findfirst(z[1, :] .>= 5)
+    @show z[1, k]
+    x_k = x[:, k]
+    w_k = w[:, k]
+    w_exact_k = w_exact[:, k]
+
+    ytick = [_scaling * (-3 + i) * 1e-3 for i in 0:7]
     xtick = [50 * i for i in 0:6]
-    p1 = Plot({color="blue"}, Coordinates(x_k, w_k))
-    p2 = Plot({}, Coordinates(x_k, w_exact_k))
-    push!(fig, {xlabel="x [km]",
-                ylabel="w [m/s]",
-                ytick = ytick,
-                xtick = xtick,
-                width="10cm",
-                height="5cm"},
-               p1, p2)
-    #x, w, w_exact = linedata[(3, 40, 10)]
-    #p1 = Plot({color="blue"}, Coordinates(x, w))
-    #p2 = Plot({}, Coordinates(x, w_exact))
-    #push!(fig, {xlabel="x [km]",
-    #            ylabel="w [m/s]",
-    #            width="10cm",
-    #            height="5cm"},
-    #           p1, p2)
-    pgfsave(joinpath(root, "gw_line.pdf"), fig)
+
+    dx = _L / KX
+
+    @pgf begin
+      p1 = Plot({dashed}, Coordinates(x_k, w_k))
+      p2 = Plot({}, Coordinates(x_k, w_exact_k))
+      push!(fig, {xlabel="x [km]",
+                  ylabel="w [m/s]",
+                  ytick = ytick,
+                  xtick = xtick,
+                  title = L"\Delta x" * " = $dx m",
+                  width="10cm",
+                  height="5cm"},
+                 p1, p2)
+    end
   end
+  pgfsave(joinpath(root, "gw_compare_line_$(N)_$(KX1)_vs_$(KX2).pdf"), fig)
 end
 
-function calculate_diagnostics(law, q, x⃗)
-  ρ, ρu, ρw, ρe = q
-  ρu⃗ = SVector(ρu, ρw)
 
-  x, z = x⃗
-  Φ = grav(law) * z
-
-  R_d = gas_constant(law)
-  p = EulerGravity.pressure(law, ρ, ρu⃗, ρe, Φ)
-  T = p / (R_d * ρ)
-
-  ρ_ref, p_ref = referencestate(law, x⃗)
-  T_ref = p_ref / (R_d * ρ_ref)
-
-  w = ρw / ρ
-  δT = T - T_ref
-
-  SVector(w, δT)
-end
 
 let
+  #outputdir = joinpath("paper_output", "gravitywave_baldauf")
   outputdir = joinpath("paper_output", "gravitywave")
+  diagnostic_data, convergence_data = calculate_diagnostics(outputdir)
 
-  convergence_data = Dict()
-  for (root, dir, files) in walkdir(outputdir)
-    jldfiles = filter(s->endswith(s, "jld2"), files)
-    length(jldfiles) == 0 && continue
-    @assert length(jldfiles) == 1
-    data = load(joinpath(root, jldfiles[1]))
+  plotdir = joinpath("paper_plots", "gravitywave")
+  mkpath(plotdir)
 
-    law = data["law"]
-    dg = data["dg"]
-    grid = dg.grid
-    cell = referencecell(grid)
-    q = data["q"]
-    qexact = data["qexact"]
-    N = size(cell)[1] - 1
-    KX = size(grid)[1]
+  #for (N, KX) in keys(diagnostic_data)
+  #  contour_plot(plotdir, diagnostic_data, N, KX)
+  #  line_plot(plotdir, diagnostic_data, N, KX)
+  #end
 
-    diag = calculate_diagnostics.(Ref(law), q, points(grid))
-    diag_exact = calculate_diagnostics.(Ref(law), qexact, points(grid))
-
-    dx = _L / size(grid)[1] / 1e3
-
-    err_w, err_T = compute_errors(dg, diag, diag_exact)
-    @show dx, err_w, err_T
-
-    if N in keys(convergence_data)
-      push!(convergence_data[N].w_errors, err_w)
-      push!(convergence_data[N].T_errors, err_T)
-      push!(convergence_data[N].dxs, dx)
-    else
-      convergence_data[N] = (dxs = Float64[], w_errors=Float64[], T_errors=Float64[])
-      push!(convergence_data[N].w_errors, err_w)
-      push!(convergence_data[N].T_errors, err_T)
-      push!(convergence_data[N].dxs, dx)
-    end
-
-    diag_points, diag = interpolate_equidistant(diag, grid)
-    _, diag_exact = interpolate_equidistant(diag_exact, grid)
-
-    contour_plot(root, diag_points, diag, diag_exact)
-  end
-
-  # sort convergence_data
-  for N in keys(convergence_data)
-    tpl = convergence_data[N]
-    tpl = sort(collect(zip(tpl...)))
-    convergence_data[N] = (dxs=[t[1] for t in tpl],
-                           w_errors = [t[2] for t in tpl],
-                           T_errors = [t[3] for t in tpl])
-  end
-  convergence_plot(outputdir, convergence_data)
+  #compare_contour_plot(plotdir, diagnostic_data, 3, (50, 100))
+  #compare_line_plot(plotdir, diagnostic_data, 3, (50, 100))
+  convergence_plot(plotdir, convergence_data)
 end
