c1cf277e829d8cc2b95785dd5e613620be30e3ef
Jeremy E Kozdon <jekozdon@nps.edu>
2022-03-14 10:21:37 -0700

    Add imex advection test

---
+++ test/advection/imex_wave_2d.jl
@@ -0,0 +1,116 @@
+using Atum
+using Atum.Advection
+
+using Test
+using Printf
+using StaticArrays: SVector
+using LinearAlgebra: norm, dot
+using WriteVTK
+
+if !@isdefined integration_testing
+  const integration_testing = parse(
+    Bool,
+    lowercase(get(ENV, "ATUM_INTEGRATION_TESTING", "false")),
+  )
+end
+
+function Atum.boundarystate(law::AdvectionLaw, n⃗, q⁻, aux⁻, _)
+    if dot(constants(law).u⃗, n⃗) ≤ 0
+        T = eltype(q⁻)
+        return SVector(zero(T)), aux⁻
+    else
+        return q⁻, aux⁻
+    end
+end
+
+function wave(law, x⃗, t)
+  T = eltype(x⃗)
+  β = 5
+  μ::T = 1 // 5
+  # FIXME: Periodic in x⃗
+  r⃗ = mod.(x⃗ .- constants(law).u⃗ .* t .+ 1, 2) .- 1
+  ρ = β * exp(- r⃗' * r⃗ / μ^2)
+  SVector(ρ)
+end
+
+function run(A, FT, N, K; volume_form=WeakForm())
+  Nq = N + 1
+
+  u⃗ = FT.((1, 0.3))
+  lawex = AdvectionLaw{FT, 2}(u⃗)
+  law = AdvectionLaw{FT, 2}((u⃗[1], FT(0)))
+  linlaw = AdvectionLaw{FT, 2}((FT(0), u⃗[2]))
+
+  cell = LobattoCell{FT, A}(Nq, Nq)
+  v1d = range(FT(-1), stop=FT(1), length=K+1)
+  grid = brickgrid(cell, (v1d, v1d); periodic=(true, false),
+                   ordering = StackedOrdering{CartesianOrdering}())
+
+  dg = DGSEM(; law, grid, volume_form,
+             surface_numericalflux = RusanovFlux())
+
+  lindg = DGSEM(; law=linlaw, grid, volume_form,
+                surface_numericalflux = RusanovFlux())
+
+  cfl = FT(1 // 4)
+  dt = cfl * step(v1d) / N / norm(constants(law).u⃗)
+  timeend = FT(1.0)
+
+  q = fieldarray(undef, law, grid)
+  q .= wave.(Ref(law), points(grid), FT(0))
+
+  @info @sprintf """Starting
+  N           = %d
+  K           = %d
+  volume_form = %s
+  norm(q)     = %.16e
+  """ N K volume_form weightednorm(dg, q)
+
+  odesolver = ARK23(dg, lindg, fieldarray(q), dt)
+  solve!(q, timeend, odesolver)
+
+  qexact = fieldarray(undef, lawex, grid)
+  qexact .= wave.(Ref(lawex), points(grid), timeend)
+  errf = weightednorm(dg, q .- qexact)
+
+  @info @sprintf """Finished
+  norm(q)      = %.16e
+  norm(q - qe) = %.16e
+  """ weightednorm(dg, q) errf
+
+  return errf
+end
+
+let
+  A = Array
+  FT = Float64
+  N = 4
+
+  expected_error = Dict()
+
+  #form, lev
+  expected_error[WeakForm(), 1] = 6.2658137389437804e-02
+  expected_error[WeakForm(), 2] = 7.7839989950141009e-03
+  expected_error[WeakForm(), 3] = 1.9527615893143112e-03
+
+  expected_error[FluxDifferencingForm(CentralFlux()), 1] = 6.2658137389437846e-02
+  expected_error[FluxDifferencingForm(CentralFlux()), 2] = 7.7839989950143299e-03
+  expected_error[FluxDifferencingForm(CentralFlux()), 3] = 1.9527615893141709e-03
+
+  nlevels = integration_testing ? 3 : 1
+
+  @testset for volume_form in (WeakForm(), FluxDifferencingForm(CentralFlux()))
+    errors = zeros(FT, nlevels)
+    for l in 1:nlevels
+      K = 5 * 2 ^ (l - 1)
+      errors[l] = run(A, FT, N, K; volume_form)
+      @test errors[l] ≈ expected_error[volume_form, l]
+    end
+    if nlevels > 1
+      rates = log2.(errors[1:(nlevels-1)] ./ errors[2:nlevels])
+      @info "Convergence rates\n" *
+        join(["rate for levels $l → $(l + 1) = $(rates[l])" for l in 1:(nlevels - 1)], "\n")
+    end
+  end
+end
+
--- test/advection/runtests.jl
+++ test/advection/runtests.jl
@@ -6,4 +6,5 @@ using SafeTestsets
   @safetestset "wave_2d" begin include("wave_2d.jl") end
   @safetestset "wave_3d" begin include("wave_3d.jl") end
   @safetestset "entropy_conservation_1d" begin include("entropy_conservation_1d.jl") end
+  @safetestset "imex_wave_2d" begin include("imex_wave_2d.jl") end
 end
