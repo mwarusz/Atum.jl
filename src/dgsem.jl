004f81a1043586c6387591ceca4bf1817a55e452
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2022-04-29 16:27:41 -0600

    Add symmetric kernel

--- src/dgsem.jl
+++ src/dgsem.jl
@@ -178,7 +178,31 @@ function launch_volumeterm(form::FluxDifferencingForm, dq, q, dg; increment, dep
         dependencies = comp_stream
       )
     end
-  else
+  elseif kernel_type == :per_dir_symmetric
+    workgroup = ntuple(i -> i <= dim ? Nq : 1, 3)
+    ndrange = (length(dg.grid) * workgroup[1], Base.tail(workgroup)...)
+    comp_stream = dependencies
+    for dir in directions(dg)
+      comp_stream = esvolumeterm_dir_symmetric!(device, workgroup)(
+        dg.law,
+        dq,
+        q,
+        derivatives_1d(cell)[1],
+        form.volume_numericalflux,
+        metrics(dg.grid),
+        dg.MJ,
+        dg.MJI,
+        dg.auxstate,
+        Val(dir),
+        Val(dim),
+        Val(Nq),
+        Val(numberofstates(dg.law)),
+        Val(Naux),
+        Val(dir == directions(dg)[1] ? increment : true);
+        ndrange,
+        dependencies = comp_stream
+      )
+    end
     error("Unknown kernel type $kernel_type")
   end
 
@@ -527,6 +551,126 @@ end
       dq[ijk, e] += dqijk
     else
       dq[ijk, e] = dqijk[:]
+    end
+  end
+end
+
+@kernel function esvolumeterm_dir_symmetric!(law,
+                                             dq,
+                                             q,
+                                             D,
+                                             volume_numericalflux,
+                                             metrics,
+                                             MJ,
+                                             MJI,
+                                             auxstate,
+                                             ::Val{dir},
+                                             ::Val{dim},
+                                             ::Val{Nq},
+                                             ::Val{Ns},
+                                             ::Val{Naux},
+                                             ::Val{increment}) where {dir, dim, Nq, Ns, Naux, increment}
+  @uniform begin
+    FT = eltype(law)
+    Nq1 = Nq
+    Nq2 = dim > 1 ? Nq : 1
+    Nq3 = dim > 2 ? Nq : 1
+  end
+
+  dqijk1 = @private FT (Ns,)
+  dqijk2 = @private FT (Ns,)
+
+  q1 = @private FT (Ns,)
+  aux1 = @private FT (Naux,)
+
+  l_g = @localmem FT (Nq ^ 3, 3)
+
+  e = @index(Group, Linear)
+  i, j, k = @index(Local, NTuple)
+
+  @inbounds begin
+    ijk = i + Nq * (j - 1 + Nq * (k - 1))
+
+    MJijk = MJ[ijk, e]
+    @unroll for d in 1:dim
+      l_g[ijk, d] = MJijk * metrics[ijk, e].g[dir, d]
+    end
+
+    @unroll for s in 1:Ns
+      dqijk1[s] = -zero(FT)
+      dqijk2[s] = -zero(FT)
+    end
+
+    @unroll for s in 1:Ns
+      q1[s] = q[ijk, e][s]
+    end
+    @unroll for s in 1:Naux
+      aux1[s] = auxstate[ijk, e][s]
+    end
+
+    source!(law, dqijk1, q1, aux1, dim, (dir,))
+    source!(law, problem(law), dqijk1, q1, aux1, dim, (dir,))
+
+    @synchronize
+
+    ijk = i + Nq * (j - 1 + Nq * (k - 1))
+
+    MJIijk = MJI[ijk, e]
+    @unroll for n in 1:Nq
+      if dir == 1
+        id = i
+        ild = n + Nq * ((j - 1) + Nq * (k - 1))
+      elseif dir == 2
+        id = j
+        ild = i + Nq * ((n - 1) + Nq * (k - 1))
+      elseif dir == 3
+        id = k
+        ild = i + Nq * ((j - 1) + Nq * (n - 1))
+      end
+
+      q2 = q[ild, e]
+      aux2 = auxstate[ild, e]
+
+      f = twopointflux(volume_numericalflux, law, q1, aux1, q2, aux2)
+      @unroll for s in 1:Ns
+        Ddn = MJIijk * D[id, n]
+        Dnd = MJIijk * D[n, id]
+        @unroll for d in 1:dim
+          dqijk1[s] -= Ddn * l_g[ijk, d] * f[d, s] / 2
+          dqijk1[s] += f[d, s] * l_g[ild, d] * Dnd / 2
+        end
+      end
+    end
+    @unroll for n in Nq:-1:1
+      if dir == 1
+        id = i
+        ild = n + Nq * ((j - 1) + Nq * (k - 1))
+      elseif dir == 2
+        id = j
+        ild = i + Nq * ((n - 1) + Nq * (k - 1))
+      elseif dir == 3
+        id = k
+        ild = i + Nq * ((j - 1) + Nq * (n - 1))
+      end
+
+      q2 = q[ild, e]
+      aux2 = auxstate[ild, e]
+
+      f = twopointflux(volume_numericalflux, law, q1, aux1, q2, aux2)
+      @unroll for s in 1:Ns
+        Ddn = MJIijk * D[id, n]
+        Dnd = MJIijk * D[n, id]
+        @unroll for d in 1:dim
+          dqijk2[s] -= Ddn * l_g[ijk, d] * f[d, s] / 2
+          dqijk2[s] += f[d, s] * l_g[ild, d] * Dnd / 2
+        end
+      end
+    end
+
+    if increment
+      dq[ijk, e] += (dqijk1 + dqijk2)
+    else
+      dq[ijk, e] = (dqijk1 + dqijk2)[:]
     end
   end
 end
